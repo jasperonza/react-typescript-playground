a4ba9874b3a20cf7d800f1130ac692e9d72c5643
jasperonza <jasperonza@gmail.com>
2024-08-04 12:30:58 +0800

    modified button component

--- button-component/src/components/Button.tsx
+++ button-component/src/components/Button.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
-
+// ***Using INTERFACE as alias ***
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string
 }
+
+// ***Using TYPE as alias***
+// type ButtonProps = React.HTMLAttributes<HTMLButtonElement> & {
+//   label: string,
+// };
 
 export const Button = ({ label, ...props }: ButtonProps) => {
   return (
