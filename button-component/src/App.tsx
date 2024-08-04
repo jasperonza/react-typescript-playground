0b4aa4fc28eefaa07c269b4f2c32cb1cce501800
jasperonza <jasperonza@gmail.com>
2024-08-04 11:24:31 +0800

    button component initial commit

---
+++ button-component/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/Button"
+
+function App() {
+
+  return (
+    <>
+      <Button className="asdf" label="click here"></Button>
+    </>
+  )
+}
+
+export default App
