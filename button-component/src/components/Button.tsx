d5bd85a7f486b788b3186d8d5863fa31bb2cae91
jasperonza <jasperonza@gmail.com>
2024-08-11 14:20:05 +0800

    added other method/s that can be used

--- button-component/src/components/Button.tsx
+++ button-component/src/components/Button.tsx
@@ -3,10 +3,27 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label?: string
 }
 
+// ***Using other method/s with INTERFACE alias***
+// Method 1
+// interface ButtonProps extends React.ComponentProps<"button"> {
+//   label?: string
+// }
+
 // ***Using TYPE as alias***
 // type ButtonProps = React.HTMLAttributes<HTMLButtonElement> & {
 //   label: string,
 // };
+
+// ***Using other method/s with TYPE alias***
+// Method 1
+// type ButtonProps = React.ComponentProps<"button"> & {
+//   label?: string
+// }
+
+// Method 2
+// type ButtonProps = React.JSX.IntrinsicElements["button"] & {
+//   label?: string
+// }
 
 export const Button = ({ label, ...props }: ButtonProps) => {
   return (
