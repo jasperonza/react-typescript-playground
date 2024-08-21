9f8cd1f1d16787c0b2ae85f16216cc97d3f71bd8
jasperonza <jasperonza@gmail.com>
2024-08-21 16:43:14 +0800

    contexts initial commit

---
+++ zblog-app/src/components/contexts/UserContext.tsx
@@ -0,0 +1,28 @@
+import { createContext, PropsWithChildren, useContext, useState } from "react";
+
+type Props = PropsWithChildren;
+
+// Create user context
+export const UserContext = createContext<any | undefined>(undefined);
+
+// User context provider
+export const UserContextProvider = ({ children }: Props) => {
+  const [user, setUser] = useState<any | undefined>(undefined);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+// Custom hook to use UserContext
+export const useUserContext = () => {
+  const user = useContext(UserContext);
+
+  if (user === undefined) {
+    throw new Error('useUserContext must be used with a UserContext');
+  }
+
+  return user;
+}
