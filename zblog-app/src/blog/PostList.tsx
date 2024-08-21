0091c482b5e095a0968102653e751a2311e3156e
jasperonza <jasperonza@gmail.com>
2024-08-21 16:22:45 +0800

    post list initail commit

---
+++ zblog-app/src/blog/PostList.tsx
@@ -0,0 +1,47 @@
+import { initializePosts } from './hooks/usePosts';
+import { usePostsQuery } from './hooks/usePostsQuery';
+import { ErrorState, Loading, NotFound } from '../components/utils/useQueryStates';
+import AppCard from '../components/app/AppCard';
+
+type Props = {}
+
+export default function PostList({ }: Props) {
+  // Render data lists
+
+  // ========================================================================
+  // *** Method 1
+  // *** Using react useState hook
+  // const [posts] = initializePosts();
+  // ========================================================================
+
+  // ========================================================================
+  // *** Method 2
+  // *** Using tanstack library
+  const { posts, isLoading, isError, error } = usePostsQuery();
+
+  if (isLoading) {
+    return <Loading />
+  }
+
+  if (isError) {
+    return <ErrorState error={error as Error} />
+  }
+
+  if (!posts) {
+    return <NotFound />
+  }
+  // ========================================================================
+
+  return (
+    <>
+      {posts && posts.map((post: any) =>
+        <AppCard key={post.id}>
+          <ul>
+            <li>{post.title}</li>
+            <li>{post.body}</li>
+          </ul>
+        </AppCard>
+      )}
+    </>
+  )
+}
