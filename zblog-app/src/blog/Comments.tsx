1e869de1a8660381a944fd898dd8e15e1afcb460
jasperonza <jasperonza@gmail.com>
2024-08-22 17:07:39 +0800

    comments initial commit

---
+++ zblog-app/src/blog/Comments.tsx
@@ -0,0 +1,12 @@
+import CommentsList from './CommentsList';
+
+type Props = {}
+
+export default function Comments({ }: Props) {
+  return (
+    <>
+      <div>Comments here</div>
+      <CommentsList />
+    </>
+  )
+}
