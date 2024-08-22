b21e35ce818fabe7282418b5072eef49f5f3b786
jasperonza <jasperonza@gmail.com>
2024-08-22 15:48:45 +0800

    modified: added react router useParams

--- zblog-app/src/blog/hooks/usePost.ts
+++ zblog-app/src/blog/hooks/usePost.ts
@@ -1,8 +1,13 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { fetchPost } from "../api/fetchPost";
 
 // Custom hook to manage post
-export const usePost = (postId: any) => {
+export const usePost = () => {
+  // React router
+  const { postId } = useParams();
+
+  // Post state
   const [post, setPost] = useState<any>(undefined);
 
   useEffect(() => {
