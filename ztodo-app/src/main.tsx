b803d20f175dcd9507e8c16e1fedc199678fd54d
jasperonza <jasperonza@gmail.com>
2024-08-27 19:01:00 +0800

    added: protected route and login route; code clean up;

--- ztodo-app/src/main.tsx
+++ ztodo-app/src/main.tsx
@@ -1,29 +1,46 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 import { UserContextProvider } from './components/contexts/UserContext.tsx';
-import App from './App.tsx';
-import HomePage from './components/pages/HomePage.tsx';
-import NotFoundPage from './components/pages/NotFoundPage.tsx';
-import './index.css';
-import TodoPage from './components/pages/TodoPage.tsx';
 import { TodoContextProvider } from './components/contexts/TodoContext.tsx';
+import App from './App.tsx';
+import LoginPage from './components/pages/LoginPage.tsx';
+import HomePage from './components/pages/HomePage.tsx';
+import TodoPage from './components/pages/TodoPage.tsx';
+import ProtectedRoute from './components/middleware/ProtectedRoute.tsx';
+import NotFoundPage from './components/pages/NotFoundPage.tsx';
 import { ToastContainer } from 'react-toastify';
+import './index.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <NotFoundPage />
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        index: true,
+        element: <LoginPage />
+      },
+      {
+        element: <ProtectedRoute element={<Outlet />} />,
+        children: [
+          {
+            path: 'home',
+            element: <HomePage />
+          },
+          {
+            path: 'todo',
+            element: <TodoPage />
+          },
+        ]
+      },
+    ]
   },
   {
-    path: '/home',
-    element: <HomePage />
+    path: 'login',
+    element: <LoginPage />,
   },
-  {
-    path: '/todo',
-    element: <TodoPage />
-  }
 ]);
 
 createRoot(document.getElementById('root')!).render(
