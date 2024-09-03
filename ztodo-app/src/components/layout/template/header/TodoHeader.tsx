44937b197832a0966f659f1977eec7be89bf87df
jasperonza <jasperonza@gmail.com>
2024-09-03 20:38:09 +0800

    modified: added user settings, code clean up

--- ztodo-app/src/components/layout/template/header/TodoHeader.tsx
+++ ztodo-app/src/components/layout/template/header/TodoHeader.tsx
@@ -1,13 +1,17 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import UserSettings from "../../../user/components/UserSettings";
 
 export default function TodoHeader() {
   return (
     <>
       <AppBar position="static" color="default">
         <Toolbar>
-          <Typography variant="h5">
-            Todo App
-          </Typography>
+          <Box sx={{ flexGrow: '1' }}>
+            <Typography variant="h5">
+              Todo App
+            </Typography>
+          </Box>
+          <UserSettings />
         </Toolbar>
       </AppBar>
     </>
